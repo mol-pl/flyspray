e7c59e5c05f6c722ad0276dee2444f3588d03b01
Eccenux <eccenux+git@gmail.com>
2021-11-16 15:11:41 +0100

    PHP7/8 compat. magic quotes

--- plugins/dokuwiki/inc/init.php
+++ plugins/dokuwiki/inc/init.php
@@ -84,16 +84,18 @@
   }
 
   // kill magic quotes
-  if (get_magic_quotes_gpc() && !defined('MAGIC_QUOTES_STRIPPED')) {
-    if (!empty($_GET))    remove_magic_quotes($_GET);
-    if (!empty($_POST))   remove_magic_quotes($_POST);
-    if (!empty($_COOKIE)) remove_magic_quotes($_COOKIE);
-    if (!empty($_REQUEST)) remove_magic_quotes($_REQUEST);
-#    if (!empty($_SESSION)) remove_magic_quotes($_SESSION); #FIXME needed ?
-    @ini_set('magic_quotes_gpc', 0);
-    define('MAGIC_QUOTES_STRIPPED',1);
+  if (PHP_MAJOR_VERSION < 7) { // not needed at least since PHP7 and removed in PHP8
+    if (get_magic_quotes_gpc() && !defined('MAGIC_QUOTES_STRIPPED')) {
+      if (!empty($_GET))    remove_magic_quotes($_GET);
+      if (!empty($_POST))   remove_magic_quotes($_POST);
+      if (!empty($_COOKIE)) remove_magic_quotes($_COOKIE);
+      if (!empty($_REQUEST)) remove_magic_quotes($_REQUEST);
+#      if (!empty($_SESSION)) remove_magic_quotes($_SESSION); #FIXME needed ?
+      @ini_set('magic_quotes_gpc', 0);
+      define('MAGIC_QUOTES_STRIPPED',1);
+    }
+    @set_magic_quotes_runtime(0);
   }
-  @set_magic_quotes_runtime(0);
   @ini_set('magic_quotes_sybase',0);
 
   // disable gzip if not available
